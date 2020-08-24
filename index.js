5597353915508113b5d9e50ad8ae3d2ebccc4496
bipon68 <bipon68@users.noreply.github.com>
2020-08-24 12:37:48 +0600

    #3 http post request handling

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const { query } = require('express');
 const app = express();
 
 const port = 4000;
+// pass all body dada by json
+app.use(express.json())
 
 app.get('/', (req, res) => {
     // const params = JSON.stringify(req.query.id)
-    res.send('Hello world!. Hello Viewers', req.query.id)
+    // res.send('Hello world!. Hello Viewers', req.query.id)
+    res.send('Hello world!. Hello Viewers')
 })
 app.get('/hello', (req, res) => {
     console.log(req.url);
     res.send('Hello Bipon')
+})
+
+app.post('/', (req, res) => {
+    const body = req.body;
+    res.send('Hello man! this is post ' + body.message);
+    // res.send('Hello you! This is post')
 })
 
 app.listen(port, () => {
@@ -20,3 +29,9 @@ app.listen(port, () => {
 })
 
 // console.log('Hello world')
+
+/**
+ * 1. up and running the express server
+ * 2. configure the express server
+ * 3. handle the routes of the server
+ */
