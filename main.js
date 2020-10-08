6def5412074462fcea3598fd9f9f14683efcd987
bipon68 <bipon68@users.noreply.github.com>
2020-10-08 19:44:54 +0600

    #10 url module

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
+const { write } = require('fs');
 var http = require('http');
+var URL = require('url');
 
 var server =  http.createServer(function (req, res){
     
     // res.end('Hello World, listen 5050 port')
 
-    if(req.url=='/'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write('<h2>This is Home page</h2>')
-        res.end();
-    }
-    else if(req.url=='/about'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write('<h2>This is About page</h2>')
-        res.end();
-    }
-    else if(req.url=='/contact'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write('<h2>This is Contact page</h2>')
-        res.end();
-    }
+    var myURL="http://bipon.me/blog.html?year=2020&month=july";
+    var myURLObj = URL.parse(myURL, true)
+    var myHostName = myURLObj.host;
+    var myPathName = myURLObj.pathname;
+    var mySearchName = myURLObj.search;
+
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(myHostName);
+    res.end();
+
+    // if(req.url=='/'){
+    //     res.writeHead(200, {'Content-Type': 'text/html'})
+    //     res.write('<h2>This is Home page</h2>')
+    //     res.end();
+    // }
+    // else if(req.url=='/about'){
+    //     res.writeHead(200, {'Content-Type': 'text/html'})
+    //     res.write('<h2>This is About page</h2>')
+    //     res.end();
+    // }
+    // else if(req.url=='/contact'){
+    //     res.writeHead(200, {'Content-Type': 'text/html'})
+    //     res.write('<h2>This is Contact page</h2>')
+    //     res.end();
+    // }
 
 });
 
