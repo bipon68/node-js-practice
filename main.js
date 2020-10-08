7e8b676d2ec47e8062d4dc2463101ff9f5504a31
bipon68 <bipon68@users.noreply.github.com>
2020-10-08 18:01:42 +0600

    #7 Request Response Model Works

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 var http = require('http');
 
 var server =  http.createServer(function (req, res){
-    res.end('Hello World, listen 5050 port')
+    
+    // res.end('Hello World, listen 5050 port')
+
+    if(req.url=='/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h2>This is Home page</h2>')
+        res.end();
+    }
+    else if(req.url=='/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h2>This is About page</h2>')
+        res.end();
+    }
+    else if(req.url=='/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h2>This is Contact page</h2>')
+        res.end();
+    }
+
 });
 
 server.listen(5050);
