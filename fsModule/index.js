ffd3097be0f5f489536b4eb76ee0d23004023e26
bipon68 <bipon68@users.noreply.github.com>
2020-10-13 21:22:12 +0600

    #13 read file sync

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,11 +4,17 @@ var server = http.createServer(function(req, res){
     if(req.url='/'){
 
         //Async
-        fs.readFile('home.html', function (error, data){
+        // fs.readFile('home.html', function (error, data){
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write(data);
+        //     res.end();
+        // })
+
+        // Sync
+        let myData = fs.readFileSync('home.html');
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
+            res.write(myData);
             res.end();
-        })
     }
 })
 
