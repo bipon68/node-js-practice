cee623d6255045d30bcad018861627e90286a6a3
bipon68 <bipon68@users.noreply.github.com>
2020-10-15 20:59:30 +0600

    #25 api route change

--- vanila-node-rest-api/server.js
+++ vanila-node-rest-api/server.js
@@ -6,9 +6,15 @@ const server =  http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/html')
     // res.write('<h2>Hello World</h2>')
     // res.end()
+    if(req.url === '/api/products'){
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(products))
+    }else{
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({message: 'Route Not Found'}))
+    }
 
-    res.writeHead(200, {'Content-Type': 'application/json'})
-    res.end(JSON.stringify(products))
+    
 })
 
 const PORT = process.env.PORT || 5000
