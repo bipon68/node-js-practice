93404d39c2027ae7ed1930a156bc37406cb7422f
bipon68 <bipon68@users.noreply.github.com>
2020-10-18 14:09:59 +0600

    #35 data insert

--- node-with-mysql/index.js
+++ node-with-mysql/index.js
@@ -14,5 +14,18 @@ con.connect(function(error){
         console.log('Node DB Connection Failed!')
     }else{
         console.log('Node DB Connection Success!')
+        InsertData(con);
     }
 })
+
+function InsertData(con){
+
+    let SQLQuery = "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('Sajib', '02', 'Five', '01913851613', 'Dhaka')"
+    con.query(SQLQuery, function(error){
+        if(error){
+            console.log('Data Insert Failed!')
+        }else{
+            console.log('Data Insert Success!')
+        }
+    })
+}
