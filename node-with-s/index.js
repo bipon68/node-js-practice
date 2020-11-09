3cc331668b0e319051b40bc9dcb20a764cb210bb
bipon68 <bipon68@users.noreply.github.com>
2020-11-09 13:58:13 +0600

    #40 module export

--- node-with-s/index.js
+++ node-with-s/index.js
@@ -1,3 +1,19 @@
 const _ = require('lodash')
+const people = require('./people')
 
 console.log(_.last([1, 2, 3]))
+
+// console.log(window)
+
+// setTimeout(() => {
+//     console.log('test')
+// }, 1000)
+
+// const a = 5;
+
+// console.log(global.a)
+
+console.log(people)
+
+
+
---
+++ node-with-s/people.js
@@ -0,0 +1,13 @@
+var people = ['sakib', 'tamim', 'bipon']
+
+var a = 6;
+
+function test(){
+    console.log("test")
+}
+console.log(module)
+module.exports = people;
+
+// (function(){
+//     var a = 5;
+// })();
